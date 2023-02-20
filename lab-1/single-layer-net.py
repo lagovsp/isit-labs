369f7cf65b977a4f13a00e43ea7eb15bac078f8a
lagovsp <lagovsp@gmail.com>
2023-02-20 04:34:19 +0300

    samples improved

--- lab-1/single-layer-net.py
+++ lab-1/single-layer-net.py
@@ -27,15 +27,13 @@ class Net:
             self.weights[i] += self.norm * delta * self.af_der(net) * sample[i]
 
     def learn_epoch(self, samples: list[list[int]]):
-        mistakes = 0
-        for sample in samples:
+        for i, sample in enumerate(samples):
             answer, net = self.predict(sample[:-1])
             delta = sample[len(self.weights)] - answer
-            mistakes += 0 if delta == 0 else 1
             self._correct_weights(net, delta, sample)
 
 
-def test_sets(net: Net, sets: list[list[int]]) -> (list[int], int):
+def test(net: Net, sets: list[list[int]]) -> (list[int], int):
     answers, mistakes = list(), 0
     for s in sets:
         net_ans, _ = net.predict(s[:-1])
@@ -44,19 +42,39 @@ def test_sets(net: Net, sets: list[list[int]]) -> (list[int], int):
     return answers, mistakes
 
 
-def learn(net: Net, sets: list[list[int]]) -> list[list]:
-    success_count, epochs, i = 0, list(), 0
+def learn(net: Net,
+          sets: list[list[int]],
+          learn_indexes: set[int],
+          test_same: bool = False) -> list[list]:
+    success_count, epochs, i, = 0, list(), 0
+    learn_sets = [sets[i] for i in learn_indexes]
+    if test_same:
+        test_sets = learn_sets
+    else:
+        test_sets = list()
+        for j in range(len(sets)):
+            if j not in learn_indexes:
+                test_sets.append(sets[j])
+
     while success_count < 1:
-        answers, mistakes = test_sets(net, sets)
+        answers, mistakes = test(net, test_sets)
         epochs.append([i, net.weights.copy(), answers.copy(), mistakes])
         success_count += 1 if mistakes == 0 else 0
-        net.learn_epoch(sets)
+        net.learn_epoch(learn_sets)
         i += 1
     return epochs
 
 
-def append_bf_val(args: list[int]) -> list[int]:
-    args.append(1 if bf(args) else 0)
+def get_append_bf_val_fun(f: Callable[[list[int]], bool]) -> Callable[[list[int]], list[int]]:
+    def append_bf_val(args: list[int]) -> list[int]:
+        args.append(1 if f(args) else 0)
+        return args
+
+    return append_bf_val
+
+
+def append_bf_val(f: Callable[[list[int]], bool], args: list[int]) -> list[int]:
+    args.append(1 if f(args) else 0)
     return args
 
 
@@ -95,7 +113,7 @@ def display_net(logs: list[list],
         logger.write(t.draw())
 
 
-bf: Callable[[list[bool]], bool] = lambda args: (args[2] and args[3]) or (not args[0]) or (not args[1])
+bf: Callable[[list[int]], bool] = lambda args: (args[2] and args[3]) or (not args[0]) or (not args[1])
 
 threshold_af_out: Callable[[float], int] = lambda net: 1 if net >= 0 else 0
 threshold_af_der: Callable[[float], float] = lambda net: 1
@@ -104,31 +122,81 @@ logistic_af_out: Callable[[float], int] = lambda net: 1 if (math.tanh(net) + 1) 
 logistic_af_der: Callable[[float], float] = lambda net: 1 / (2 * (math.cosh(net) ** 2))
 
 inputs = list(map(append_left_true,
-                  list(map(append_bf_val,
+                  list(map(get_append_bf_val_fun(bf),
                            list(map(list,
                                     list(product([0, 1], repeat=4))))))))
 
 
+def truthtable(f: Callable[[list[int]], bool], n: int) -> str:
+    args = list(map(get_append_bf_val_fun(f),
+                    list(map(list,
+                             list(product([0, 1], repeat=n))))))
+    args.insert(0, [f'x{i}' for i in range(1, n + 1)] + ['f'])
+
+    t = Texttable()
+    t.set_chars(['—', '|', '+', '—'])
+    t.set_deco(Texttable.BORDER | Texttable.HEADER | Texttable.VLINES)
+    t.add_rows(args)
+    return t.draw()
+
+
 def main():
     global inputs
+    time = custom_time_str()
+    var = 6
 
-    net_threshold = Net(weights_num=len(inputs[0]) - 1,
-                        norm=0.3,
-                        af=threshold_af_out,
-                        af_der=threshold_af_der,
-                        name='threshold')
-    display_net(learn(net_threshold, inputs),
-                to_file=True,
-                file_name=f'{net_threshold.name}-{custom_time_str()}')
+    with open(f'truthtable-var-{var}.dat', 'w') as logger:
+        logger.write(truthtable(bf, 4))
 
-    net_logistic = Net(weights_num=len(inputs[0]) - 1,
-                       norm=0.3,
-                       af=logistic_af_out,
-                       af_der=logistic_af_der,
-                       name='logistic')
-    display_net(learn(net_logistic, inputs),
+    # All sets used
+    net_threshold_all = Net(weights_num=5,
+                            norm=0.3,
+                            af=threshold_af_out,
+                            af_der=threshold_af_der,
+                            name='threshold-all-learn')
+    display_net(learn(net_threshold_all,
+                      inputs,
+                      set(range(len(inputs))),
+                      test_same=True),
                 to_file=True,
-                file_name=f'{net_logistic.name}-{custom_time_str()}')
+                file_name=f'{net_threshold_all.name}-{time}')
+
+    net_logistic_all = Net(weights_num=5,
+                           norm=0.3,
+                           af=logistic_af_out,
+                           af_der=logistic_af_der,
+                           name='logistic-all-learn')
+    display_net(learn(net_logistic_all,
+                      inputs,
+                      set(range(len(inputs))),
+                      test_same=True),
+                to_file=True,
+                file_name=f'{net_logistic_all.name}-{time}')
+
+    # Part learning
+    net_threshold_part = Net(weights_num=5,
+                             norm=0.3,
+                             af=threshold_af_out,
+                             af_der=threshold_af_der,
+                             name='threshold-part-learn')
+    # display_net(learn(net_threshold_part,
+    #                   inputs,
+    #                   {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 12, 13},
+    #                   test_same=False),
+    #             to_file=True,
+    #             file_name=f'{net_threshold_part.name}-{time}')
+
+    net_logistic_part = Net(weights_num=5,
+                            norm=0.3,
+                            af=logistic_af_out,
+                            af_der=logistic_af_der,
+                            name='logistic-part-learn')
+    # display_net(learn(net_logistic_part,
+    #                   inputs,
+    #                   {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 12, 13},
+    #                   test_same=False),
+    #             to_file=True,
+    #             file_name=f'{net_logistic_part.name}-{time}')
 
 
 if __name__ == '__main__':
